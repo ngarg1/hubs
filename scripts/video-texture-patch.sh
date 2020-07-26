035b4f5ad75d22c1f6c8267393ec1b871a6b11ca
Doug Rudolph <drudolph914@gmail.com>
2020-07-26 16:33:44 -0400

    added hacky script to download custom patch for threejs video texture -- (the patch is only applied to the production version of threejs)

---
+++ scripts/video-texture-patch.sh
@@ -0,0 +1,12 @@
+if [ -d "./node_modules/" ]
+then
+  echo "downloading patch"
+
+  # remove old vid texture file
+  rm ./node_modules/build/three.module.js
+
+  # download custom patched file
+  curl https://raw.githubusercontent.com/honeyimholm/three.js/hubs/master/three.module.js > ./node_modules/three/build/three.module.js
+
+  echo "done downloading"
+fi
